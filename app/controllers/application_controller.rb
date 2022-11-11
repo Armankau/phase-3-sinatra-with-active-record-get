013313363f1dcbf524eb6d1751169592758878e8
+ <+>
2022-11-11 13:40:06 -0800

    commit tree 622f2254bd6183ac99e1bcf5eb2e1a71111df990

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  set :default_content_type, 'application/json'
+
+
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+    # send associated reviews
+    game.to_json(only: [:id, :titile, :genre, :price],include: {reviews: {include: :user} })
+  end
+
 end
